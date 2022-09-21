f7d281c744b4dbe00198ec323ea8046facfdf9c2
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-09-21 14:16:31 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ * print_alphabet_x10 - prints the alphabets 10 times
+ *
+ * Return: void
+ *
+ */
+
+void print_alpahbet_x10(void)
+{
+	char alpha;
+	int count;
+
+	for (count = 1; count <= 10; count++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
