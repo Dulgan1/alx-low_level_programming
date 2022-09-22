e624b038433b0152bfa010c6075c95879ed31e34
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-09-22 02:05:18 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,18 +10,18 @@ int print_last_digit(int a)
 {
 	if (a > 10)
 	{
-		_putchar(a % 10);
+		_putchar('0' + (a % 10));
 		return (a % 10);
 	}
 	else if (a < 0)
 	{
 		a = a * -1;
-		_putchar(a % 10);
+		_putchar('0' + (a % 10));
 		return (a % 10);
 	}
 	else
 	{
-		_putchar(a);
+		_putchar('0' + a);
 		return (a);
 	}
 }
