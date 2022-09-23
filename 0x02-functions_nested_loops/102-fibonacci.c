286841a39c34199eafe26f21311b9ef8c2660f94
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-09-23 13:19:58 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
 /**
- * main - entry of tge program
- *
- * Return: always 0
+ * main - print first 50 Fibonacci numbers
+ * Return: 0
  */
 
 int main(void)
 {
-	long n = 1;
-	long m = 2;
-	int cnto = 50;
-	int cntr;
+	int counter;
+	int countto = 50;
+	long a = 1;
+	long b = 2;
 
-	for (cntr = 1; cntr <= (cnto / 2); cntr++)
+	for (counter = 1; counter <= (countto / 2); counter++)
 	{
-		printf("%li %li", n, m);
-		n += m;
-		m += n;
+		printf("%li %li ", a, b);
+		a += b;
+		b += a;
 	}
-	if (cnto % 2 == 1)
-		printf("%li", n);
+	if (countto % 2 == 1)
+		printf("%li", a);
+
 	printf("\n");
-	
+
 	return (0);
 }
