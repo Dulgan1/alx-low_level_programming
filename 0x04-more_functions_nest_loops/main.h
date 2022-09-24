0b596e9cf91823c304da8a850482f3bd59e7b499
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-09-23 17:31:27 -0700

    0x04 commits

---
+++ 0x04-more_functions_nest_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * isupper - evaluates an alphabet if upper or lower
+ *
+ * Return: int 1 for upper and 0 for lower case 
+ */
+
+int _isupper(char);
+
+#endif
