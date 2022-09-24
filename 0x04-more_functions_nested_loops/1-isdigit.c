b57474d5bbaba0c5fe969d167b9e670a6f64ead6
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-09-23 17:55:53 -0700

    0x04 commits

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _isdigit - checks for a digit 0 through 9
@@ -9,7 +9,9 @@
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
+	{
 		return (1);
+	}
 
 	return (0);
 }
