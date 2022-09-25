22835f0b7567422e235c21b11458b9838a9e25a3
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-09-25 16:24:56 -0700

    Initial commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * positive_or_negative - checks if a number is positive or negative
+ *
+ * Return: void
+ */
+
+void positive_or_negative(int);
+
+#endif
