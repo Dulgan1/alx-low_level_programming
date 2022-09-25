b3381703557cc7c88f1b6adeea41cf7fbcb3cc56
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-09-25 07:34:38 -0700

    0x04 commits

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -35,4 +35,12 @@ int mul(int, int);
 
 void print_numbers(void);
 
+/**
+ * print_most_numbers - pru ts from 0 to 9 except 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void);
+
 #endif
