c789b55e478334aca3bf7de9f548ca3a3afd151e
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-09-25 17:34:58 -0700

    Initial commit

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * reset_to_98 - takse a pointer to an int as argument and updates the value it ponts to to 98
+ * @n: pointer to n
+ * Return: void
+ *
+ */
+
+void reset_to_98(int);
+
+#endif
