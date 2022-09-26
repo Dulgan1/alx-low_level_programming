cc43ff480e96291db2ddc8f30f750355472b8bc6
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-09-25 17:46:00 -0700

    Initial commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,4 +10,13 @@
 
 void reset_to_98(int *n);
 
+/**
+ * swap_int - swaps the value of two integers
+ * @a: first int
+ * @b: secomd int
+ * Return: void
+ */
+
+void swap_int(int *a, int *b);
+
 #endif
