1f469999ce9f7d21d3b74b55546279416280e4f6
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-09-25 17:51:30 -0700

    Inital commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -19,4 +19,12 @@ void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
 
+/**
+ * _strlen - returns the length of sa string
+ * @s: pointer to string
+ * Return: int the length
+ */
+
+int _strlen(char *s);
+
 #endif
