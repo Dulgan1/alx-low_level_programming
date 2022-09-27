bf7ce9a378a2ab5a1a90651467981ff63a266256
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-09-27 15:22:02 -0700

    0x07 commits

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -17,11 +17,11 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 	else
 	{
-		if (size > 0)
+		if (n > 0)
 		{
 			int i;
 
-			for (i = 0; i < size; i++)
+			for (i = 0; i < n; i++)
 				s[i] = b;
 		}
 	}
