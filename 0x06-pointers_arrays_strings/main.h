d225d07aaae5a0217071061d50df17f6eade338e
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-09-27 01:41:44 -0700

    0x06 commits

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,4 +11,16 @@
 
 char *_strcat(char *dest, char *src);
 
+/**
+ * _strncat - concatenate n bytes to dest strinv
+ *
+ * @dest: destination string
+ *
+ * @src: source of string
+ *
+ * Return: char
+ */
+
+char *_strncat(char *dest, char *src, int n);
+
 #endif
