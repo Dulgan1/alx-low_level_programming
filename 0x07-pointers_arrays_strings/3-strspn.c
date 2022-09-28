1d9da22a71cedce5e4774b0ffde631e01304369e
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-09-28 13:00:38 -0700

    0x07 commits

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,16 +12,16 @@ unsigned int _strspn(char *s, char *accept)
 	int i = 0, j;
 	int matches = 0;
 
-	while (*(s + i))
+	while (s[i] != '\0')
 	{
-		for (j = 0; *(accept + j); j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
 			{
 				matches++;
 				break;
 			}
-			if (*(accept + j + 1) == '\0' && *(s + i) != *(accept + j))
+			if (accept[j + 1] == '\0' && *(s + i) != *(accept + j))
 				return (matches);
 		}
 		i++;
