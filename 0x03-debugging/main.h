a108342c88a4bcc0812ed4376c9996359db9b5d7
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-02 12:41:00 -0700

    task 2 initial commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -9,4 +9,18 @@
 
 void positive_or_negative(int);
 
+#include"main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ *
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ *
+ * Return: largest number
+ */
+
+int largest_number(int, int, int);
+
 #endif
