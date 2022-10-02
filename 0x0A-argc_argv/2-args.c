13e758e11a7b06e980f311f0e8bd47a7eb9fcc73
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-02 08:20:07 -0700

    Init commit for 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry of program, prints all command line arguments
+ * @argc: counts of argv elements
+ * @argv: vector array containing command line arguments passed wuth tge program
+ * Return: int 0 on successful run
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc > 0)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
+	}
+
+	return (0);
+}
