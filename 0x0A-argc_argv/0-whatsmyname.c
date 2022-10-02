80761fbf280e71b321cf7ba74a155b742e4893ed
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-02 06:14:09 -0700

    Initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry of program, prints the name of tge name of the program
+ * @argc: counts of elements in argv
+ * @argv: command line arguments stored as string in array
+ * Return: 0 on successful program
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+0x0A agrc and argv readme
