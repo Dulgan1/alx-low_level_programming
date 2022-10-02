b0bace2ce5e7af627f457875543efa4ee75a02e3
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-02 08:44:57 -0700

    Initial commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry of program c, command line argument and multiplies them
+ * @argc: counts of elements of argv array
+ * @argv: string array
+ * Return: int 0 for successful code run.
+ */
+
+int main(int argc, char *argv[])
+{
+	int x;
+	int y;
+
+	if (argc < 3 || argc > 3)
+		printf("Error\n");
+	else
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+
+		mul = x * y;
+
+		printf("%d\n", mul);
+	}
+}
