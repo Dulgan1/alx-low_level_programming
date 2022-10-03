6f60426f9f19bca62892e28f63fd4633bde83f15
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-03 16:03:49 -0700

    Comforming to Betty

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,34 +5,36 @@
  * _strdup - returns pointer of newly allocated
  * and copies string passed to it
  * @str: passed string to be copied
+ *
+ * Return: memory location of new string
  */
 
-char *_strdup(char* str)
+char *_strdup(char *str)
 {
 	int i = 0;
 	int len = 0;
 	int j;
 	char *p;
 
+	if (str == NULL)
+		return (NULL);
+
 	while (*(str + i))
 	{
 		len++;
 		i++;
 	}
+
 	len++;
-
-	if (str == NULL)
-		return (NULL);
-
 	p = malloc(sizeof(char) * len);
+
 	if (p == NULL)
 		return (NULL);
+
 	for (j = 0; j <= len; j++)
 	{
 		*(p + j) = *(str + j);
-		/*if (i == len)
-		*	*(p + j) = '\0';
-		*/
 	}
+
 	return (p);
 }
