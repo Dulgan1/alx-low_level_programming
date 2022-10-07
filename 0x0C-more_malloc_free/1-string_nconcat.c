a61f3e381b801dbd713e83f63c3006ecd267f3a9
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-07 04:48:48 -0700

    some tewaks 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,16 +6,14 @@
  * @n: first n chars of s2 to concatenate with s2
  * @s1: first string
  * @s2: second string
- * Return: pointer tonnew allocated memory for the concatenated string
+ * Return: pointer to new allocated memory for the concatenated string
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int i = 0;
-	unsigned int len1 = 0; /* For length of s1 */
-	unsigned int len2 = 0; /* For length of s2 */
-	unsigned int j = 0;
+	unsigned int i = 0, len1 = 0; /* For length of s1 */
+	unsigned int len2 = 0, j = 0; /* For length of s2 */
 
 	if (s1 == NULL)
 	{
@@ -35,11 +33,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = len2;
 	else
 		len2 = n;
-	i = 0;
-	j = 0;
-
+	i = 0, j = 0;
 	p = malloc(sizeof(*p) * (len1 + len2)); /* allocates memory */
-
 	if (p == NULL)
 		return (NULL);
 	while (i < len1) /* copying s1 to allocatrd memory*/
@@ -51,8 +46,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (j < len2) /* add s2 to allocated memory after copying s1 */
 	{
 		*(p + i) = *(s2 + j);
-		j++;
-		i++;
+		j++, i++;
 	}
 	*p += '\0';
 
