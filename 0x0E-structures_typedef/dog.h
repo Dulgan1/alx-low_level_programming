d0fea036d8af1c7a3cd554fb4ccb22d1b2400332
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-10 16:12:58 -0700

    Added prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,15 @@ struct dog
 	char *owner;
 };
 
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: address of datatype struct dog to be initialized
+ * @name: member of struct dog to give name
+ * @age: member of struct dog to give age
+ * @owner: member of struct dog to give onwer name
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
