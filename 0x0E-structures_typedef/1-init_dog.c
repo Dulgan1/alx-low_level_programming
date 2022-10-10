84bf4cea1eec2eb20c119b42c6a9b95fd0fe1a4d
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-10 16:15:16 -0700

    Added standard library

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - initializes a variable of type struct dog
@@ -11,7 +12,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = mallpc(sizeof(struct dog));
+	d = malloc(sizeof(struct dog));
 
 	d->name = name;
 	d->age = age;
