c9a246dc67541052aa2453490d3da51a7b832efd
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-10 15:47:01 -0700

    Initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: of the dog
+ * Description: struct describes a dog and its owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
