08835c83c37e040c5bf37cff97bc727ec84b95b4
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-10 16:04:29 -0700

    Initial commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: address of datatype struct dog to be initialized
+ * @name: member of struct dog to give name
+ * @age: member of struct dog to give age
+ * @owner: member of struct dog to give onwer name
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = mallpc(sizeof(struct dog));
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
