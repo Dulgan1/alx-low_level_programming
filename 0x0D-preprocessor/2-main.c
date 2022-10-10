17c40fb38771991e0eb76f3247163bba3f76e152
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-10 09:12:41 -0700

    Initial commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file compiled from
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
