7f948246f7db4f0d0d07cdef860b6d33e458a67d
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-11 15:41:36 -0700

    Fixing 4-new_dog.c: Copying string

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,13 +12,31 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	int i, len1, len2;
+	i = 0, len1 = 0, len2 = 0;
 	dog = malloc(sizeof(dog_t));
 
 	if (dog == NULL)
+	{
+		free(dog);
 		return (NULL);
-	dog->name = name;
+	}
+	while (name[i] != '\0')
+	{
+		len1++;
+		i++;
+	}
+	i = 0;
+	while (owner[i] != '\0')
+	{
+		len2++;
+		i++;
+	}
+	for (i = 0; i <= len1; i++)
+		dog->name[i]  = name[i];
 	dog->age = age;
-	dog->owner = owner;
+	for (i = 0; i <= len2; i++)
+		dog->owner[i] = owner[i];
 
 	return (dog);
 }
