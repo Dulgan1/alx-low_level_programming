147dc0eb877e5da192e3cf0477653e769aa08c7c
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-11 14:23:48 -0700

    Fixing 2-print_dog.c: conforming to betty

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,19 +12,17 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
 	else
-	{
-		if (d->name == NULL)
-			printf("Name: (nil)\n");
-		else
-			printf("Name: %s\n", d->name);
-		if (d->age)
-			printf("Age: %.6f\n", d->age);
-		else
-			printf("Age: (nil)\n");
-		if (d->owner == NULL)
-			printf("Owner: (nil)");
-		else
-			printf("Owner: %s\n", d->owner);
-	}
+		printf("Name: %s\n", d->name);
+	if (d->age)
+		printf("Age: %.6f\n", d->age);
+	else
+		printf("Age: (nil)\n");
+	if (d->owner == NULL)
+		printf("Owner: (nil)");
+	else
+		printf("Owner: %s\n", d->owner);
 }
