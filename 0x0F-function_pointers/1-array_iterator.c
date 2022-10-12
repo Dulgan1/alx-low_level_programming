f574d536b7f29779b07516687d7f136e2cc4b011
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-12 13:59:54 -0700

    Initial commit for 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function that executes a passed function on elements of ar
+ * @array: integer array
+ * @size: size of array
+ * @action: function to execute
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			(*action)(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -9,5 +9,13 @@
  */
 
 void print_name(char *name, void (*f)(char *));
+/**
+ * array_iterator - function that executes a passed function on elements of ar
+ * @array: integer array
+ * @size: size of array
+ * @action: function to execute
+ * Return: void
+ */
 
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
