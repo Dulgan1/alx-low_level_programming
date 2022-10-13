a5866a078c33fa1c5a0f97e310473bf5ff41abee
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-13 13:57:43 -0700

    Initial commit for 1-print_numbers.h

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints all numbers from arguments of variadic function
+ * @separator:  separates numbers in ouyput
+ * @n: number of  arguments  baze argument
+ * Returnvoid
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ptr;
+
+	if (n > 0)
+	{
+		va_start(ptr,n);
+		for (i = 0; i <= n; i++)
+		{
+			printf("%d", va_arg(ptr, int));
+			if (i != n && separator != NULL)
+				printf("%s", separator);
+
+		}
+		va_end(ptr);
+	}
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,4 +9,12 @@
 
 int sum_them_all(int n, ...);
 
+/**
+ * print_numbers - prints all numbers from arguments of variadic function
+ * @separator:  separates numbers in ouyput
+ * @n: number of  arguments  baze argument
+ * Returnvoid
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
