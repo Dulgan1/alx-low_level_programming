f474dd4470ef01e489fd87dd219082499f1983f8
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-20 16:50:23 -0700

    Conforming to betty

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * struct list_s - singly linked list
+ * struct list_t - singly linked list
  * @str: malloc'ed string
  * @len: length of string
  * @next: points to the next node
