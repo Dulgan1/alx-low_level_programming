f0fbd3f5761da09d04924722a565bacc653598db
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-21 06:20:44 -0700

    Initial commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len -  returns number nodes in a linked list
+ * @h: head of linked list
+ * Return: in size_t the num of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+}
