d74f94913bed58e3a2e8a41fc1d0d8302a528919
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-24 08:02:22 -0700

    Finished 2, conforming to betty

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,12 +2,34 @@
 
 /**
  * add_nodeint - adds a node
- * @head: head node 
+ * @head: head node
  * @n: for n data in node
  * Return: pointer to node added
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+	listint_t *newNode;
+	const int newn = n;
+	const int  *p;
 
+	p = &n;
+
+	if (p == NULL)
+		return (NULL);
+
+	newNode = (listint_t *)(malloc(sizeof(listint_t)));
+
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = newn;
+
+	if (head == NULL)
+		newNode->next = NULL;
+	else
+		newNode->next = *head;
+	*head = newNode;
+
+	return (newNode);
 }
