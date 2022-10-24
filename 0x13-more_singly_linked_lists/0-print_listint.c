ee3d0d5e8896df6ed3371b3030491d70f7fcf760
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-24 04:34:10 -0700

    Initial Commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all nodes of listint_t linked list
+ * @h: head pointer
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t cnt = 0;
+
+	while (h != NULL)
+	{
+		 printf("%d\n", h->n);
+		 cnt++;
+		 h = h->next;
+	}
+
+	return (cnt);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+## 0x13 Mor Singly Linked lists project
