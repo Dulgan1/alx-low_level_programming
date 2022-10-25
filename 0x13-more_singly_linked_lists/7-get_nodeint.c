4ed460eced6dee5e392389682099bf0f111e078d
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-25 15:29:00 -0700

    Conforming to betty

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,12 +3,14 @@
 /**
  * get_nodeint_at_index - gets the node at the index passed
  * @head: linked list (head node)
+ * @index: the index
  * Return: node at index
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int cnt = 0;
+
 	if (head == NULL)
 		return (NULL);
 	while (head != NULL)
