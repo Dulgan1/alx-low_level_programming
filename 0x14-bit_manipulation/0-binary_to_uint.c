226c6475da86d13acaff3c8c8b98ea4f2bcbe2b8
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-27 16:08:29 -0700

    Conforming to betty and error checking

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,24 +2,27 @@
 
 /**
  *  binary_to_uint - converts binary to unsigned integer
- *  @b: string of 0's and 1s                                                      *  Return: unsigned integer
+ *  @b: string of 0's and 1s
+ *  Return: unsigned integer
  */
 
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n = 0, i = 0, j = 0;
 
+	if (b == NULL)
+		return (n);
 	while (*(b + i) != '\0')
 		i++;
 	while (*(b + j))
 	{
 		if (*(b + j) != '1' && *(b + j) != '0')
-			return (n/2);
+			return (n / 2);
 		if (*(b + j) == '1')
 			n += (1 * (1 << i));
 		j++;
 		i--;
 	}
 
-	return (n/2);
+	return (n / 2);
 }
