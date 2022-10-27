f5b192d7e88b24aee031bb8f1bd8343edb66b5e7
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-27 16:02:46 -0700

    Initial Commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *  binary_to_uint - converts binary to unsigned integer
+ *  @b: string of 0's and 1s                                                      *  Return: unsigned integer
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0, i = 0, j = 0;
+
+	while (*(b + i) != '\0')
+		i++;
+	while (*(b + j))
+	{
+		if (*(b + j) != '1' && *(b + j) != '0')
+			return (n/2);
+		if (*(b + j) == '1')
+			n += (1 * (1 << i));
+		j++;
+		i--;
+	}
+
+	return (n/2);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  binary_to_uint - converts binary to unsigned integer
+ *  @b: string of 0's and 1s
+ *  Return: unsigned integer
+ */
+
+unsigned int binary_to_uint(const char *b);
+#endif
