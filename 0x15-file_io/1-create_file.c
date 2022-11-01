5e6e5c0d4b097e07e64d6bff523166b60da0aa17
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-10-31 17:26:58 -0700

    Initial commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _strlen - coints the length of a string
+ * @s: string to be count
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+ * create_file - creates a new file
+ * @filename: name for the file
+ * @text_content: as name implies
+ * Return: 1 on succes, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nwrite;
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd < 0)
+		return (-1);
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+	nwrite = write(fd, text_content, _strlen(text_content));
+	if (nwrite < 0 || nwrite != _strlen(text_content))
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,4 +15,21 @@
  */
 
 ssize_t read_textfile(const char *filename, size_t letters);
+
+/**
+ * create_file - creates a new file
+ * @filename: name for the file 
+ * @text_content: as name implies
+ * Return: 1 on succes, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content);
+
+/**
+ * _strlen - coints the length of a string
+ * @s: string to be count
+ * Return: length
+ */
+
+int _strlen(char *s);
 #endif
