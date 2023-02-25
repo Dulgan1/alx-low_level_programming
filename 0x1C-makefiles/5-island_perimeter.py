9d57bf0a626050f969887e488985ee15184021ce
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-02-25 00:01:25 -0800

    Docummented function

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """
  5-island_perimeter
+ Technical interview practice:
  A grid containing 1s and 0s represents and island
  where 1 is a square cell of land with sides of length 1, and 0 of water.
  This function calculates the perimeter of the island.
- Use with 5-main.py.
+ Use with  test file 5-main.py, default grid perimeter is 12,
+ tweak grid for different perimeter.
 """
 def island_perimeter(space):
+    """ Function to get perimeter"""
     pmetre = 0
     for x in range(len(space)):
         for y in range(len(space[x])):
