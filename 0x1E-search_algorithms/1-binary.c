8e313cbc04948a2359ae7215f663a1024de90d9a
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-07-12 19:22:54 +0100

    Corrected algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -21,14 +21,14 @@ int binary_search(int *array, size_t size, int value)
 	while (low <= high)
 	{
 		print_array(array, low, high);
-		mid = (high + low)/2;
+		mid = (high + low) / 2;
 		if (array[mid] == value)
 			return (mid);
 
 		else if (array[mid] > value)
-			low = mid + 1;
-		else
 			high = mid - 1;
+		else
+			high = mid + 1;
 	}
 
 	return (-1);
