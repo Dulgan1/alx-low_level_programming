544620ce9d27677c4a9b87ed8470a3bb59c79b6c
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-07-12 17:41:21 +0100

    comforming to code style

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,8 +5,10 @@
 /**
  * linear_search - linear search algorithm that works on linear array
  * @array: array to search through
- * @size: size of array                                                        * @value: value to search in array
- * Return: returns -1 of value is not found, returns index of value            */
+ * @size: size of array
+ * @value: value to search in array
+ * Return: returns -1 of value is not found, returns index of value
+ */
 
 int linear_search(int *array, size_t size, int value);
 #endif
